a7ec7b75d93d1c91054ce0c5046c0acd2cc397ab
Helic-al <helihelical@gmail.com>
2026-03-26 22:02:30 +0900

    fixed macdClass

--- pyBot/dynamicMACD.py
+++ pyBot/dynamicMACD.py
@@ -9,50 +9,52 @@ class Cross(IntEnum):
 
 class DynamicMACD:
     def __init__(self, shortSpan=12, longSpan=26, signalSpan=9):
-        self.shortAlpha = 2 / (shortSpan + 1)
-        self.longAlpha = 2 / (longSpan + 1)
-        self.signalAlpha = 2 / (signalSpan + 1)
+        self._shortAlpha = 2 / (shortSpan + 1)
+        self._longAlpha = 2 / (longSpan + 1)
+        self._signalAlpha = 2 / (signalSpan + 1)
 
-        self.emaShort = None
-        self.emaLong = None
-        self.macd = None
-        self.signal = None
-        self.hist = 0.0
-        self.prevHist = 0.0
+        self._emaShort = None
+        self._emaLong = None
+        self._macd = None
+        self._signal = None
+        self._hist = 0.0
+        self._prevHist = 0.0
 
-        self.count = 50
+        self._count = 50
 
     def update(self, price):
-        if self.emaShort is None:
-            self.emaShort = price
-            self.emaLong = price
-            self.macd = 0.0
-            self.signal = 0.0
+        if self._emaShort is None:
+            self._emaShort = price
+            self._emaLong = price
+            self._macd = 0.0
+            self._signal = 0.0
 
             return
 
-        self.prevHist = self.hist
+        self._prevHist = self._hist
 
         # EMAの更新
-        self.emaShort = self.shortAlpha * price + (1 - self.shortAlpha) * self.emaShort
-        self.emaLong = self.longAlpha * price + (1 - self.longAlpha) * self.emaLong
+        self._emaShort = (
+            self._shortAlpha * price + (1 - self._shortAlpha) * self._emaShort
+        )
+        self._emaLong = self._longAlpha * price + (1 - self._longAlpha) * self._emaLong
 
         # MACD, signal
-        self.macd = self.emaShort - self.emaLong
-        self.signal = (
+        self._macd = self._emaShort - self._emaLong
+        self._signal = (
             self.signalAlpha * self.macd + (1 - self.signalAlpha) * self.signal
         )
-        self.hist = self.macd - self.signal
+        self._hist = self.macd - self.signal
 
         # カウントの更新、最初の50レコードはクロス判定しない
-        self.count = self.count - 1 if self.count > 0 else 0
+        self._count = self.count - 1 if self.count > 0 else 0
 
     def crossDetection(self):
-        if self.prevHist <= 0 and self.hist > 0 and self.count <= 0:
+        if self._prevHist <= 0 and self.hist > 0 and self.count <= 0:
             # ゴールデンクロス
             return int(Cross.goldenCross)
 
-        elif self.prevHist >= 0 and self.hist < 0 and self.count <= 0:
+        elif self._prevHist >= 0 and self.hist < 0 and self.count <= 0:
             # デッドクロス
             return int(Cross.deadCross)
 
@@ -62,35 +64,35 @@ class DynamicMACD:
     @property
     def emaShort(self):
         """The  property."""
-        if self.emashort:
-            return self.emaShort
+        if self._emaShort:
+            return self._emaShort
         else:
             return 0.0
 
     @property
     def emaLong(self):
-        if self.emaLong:
-            return self.emaShort
+        if self._emaLong:
+            return self._emaLong
         else:
             return 0.0
 
     @property
     def macd(self):
-        if self.macd:
-            return self.macd
+        if self._macd:
+            return self._macd
         else:
             return 0.0
 
     @property
     def signal(self):
-        if self.signal:
-            return self.signal
+        if self._signal:
+            return self._signal
         else:
             return 0.0
 
     @property
     def hist(self):
-        if self.hist:
-            return self.hist
+        if self._hist:
+            return self._hist
         else:
             return 0.0
